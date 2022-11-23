72f90ba46af5a28c22250b44e070166ef6b7e3ea
KacperGil <kacpergil1610@gmail.com>
2022-11-23 18:57:20 +0100

    forek z tablica

--- src/For.java
+++ src/For.java
@@ -1,7 +1,8 @@
 public class For {
     public static void main(String[] args) {
-        for(int i=0; i<10; i++){
-            System.out.println(i);
+        String[] cats=new String[]{"mruczek", "burek", "iskierka", "pussy"};
+        for(int i=0; i<cats.length; i++){
+            System.out.println(cats[i]);
         }
     }
 }
