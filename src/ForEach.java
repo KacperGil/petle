560964db0538d6cc55f9a17f33e9803ab36e30a2
KacperGil <kacpergil1610@gmail.com>
2022-11-23 19:23:51 +0100

    forekeach pierwszy

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cats=new String[]{"mruczek", "burek", "iskierka", "pussy"};
+        for(String cat:cats){
+            System.out.println(cat);
+        }
+    }
+}
